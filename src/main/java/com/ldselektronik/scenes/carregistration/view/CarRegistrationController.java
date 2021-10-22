94a066d683a95413d677bd4d59de37228ba7a50e
Baran Buzluk <baranbuzluk@gmail.com>
2021-10-22 23:08:22 +0300

    CarRegistrationController fieldların isimlendirmeleri değiştirildi.

--- src/main/java/com/ldselektronik/scenes/carregistration/view/CarRegistrationController.java
+++ src/main/java/com/ldselektronik/scenes/carregistration/view/CarRegistrationController.java
@@ -13,84 +13,85 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputControl;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class CarRegistrationController {
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, String> licenseColumn;
+	private TableColumn<CarRegistrationDTO, String> columnLicense;
 
 	@FXML
-	private TextField idField;
+	private TextField fieldName;
 
 	@FXML
-	private TextField companyNameField;
+	private Button btnSearch;
 
 	@FXML
-	private ComboBox<CarBrandDTO> brandCBox;
+	private TableColumn<CarRegistrationDTO, String> columnCompany;
+
+	@FXML
+	private ComboBox<CarBrandDTO> cboxBrand;
 
 	@FXML
 	private AnchorPane rootPane;
 
 	@FXML
-	private TextField nameField;
+	private TableColumn<CarRegistrationDTO, Integer> columnId;
 
 	@FXML
-	private TextField dateField;
+	private TextField fieldSurname;
 
 	@FXML
-	private Button recordDeleteButton;
+	private TableView<CarRegistrationDTO> tableRegistration;
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, String> companyColumn;
+	private TextField fieldDate;
 
 	@FXML
-	private TextField carLicenseField;
+	private TextField fieldDocumentNo;
 
 	@FXML
-	private TextField phoneField;
+	private TextField fieldCompanyName;
 
 	@FXML
-	private Button recordSaveButton;
+	private Button btnRegister;
 
 	@FXML
-	private Button recordRefreshButton;
+	private Button btnRecordRefresh;
 
 	@FXML
-	private TextField surnameField;
+	private TableColumn<CarRegistrationDTO, CarBrandDTO> columnBrand;
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, String> documentNoColumn;
+	private Button btnDelete;
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, String> nameColumn;
+	private TextField fieldCarLicense;
 
 	@FXML
-	private Button recordSearchButton;
+	private TableColumn<CarRegistrationDTO, String> columnDocumentNo;
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, Date> dateColumn;
+	private TableColumn<CarRegistrationDTO, Date> columnDate;
 
 	@FXML
-	private TableView<CarRegistrationDTO> registrationTableView;
+	private TextField fieldPhone;
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, CarBrandDTO> brandColumn;
+	private TableColumn<CarRegistrationDTO, String> columnName;
 
 	@FXML
-	private TableColumn<CarRegistrationDTO, Integer> idColumn;
-
-	@FXML
-	private TextField documentNoField;
+	private TextField fieldId;
 
 	public CarRegistrationController() {
 		loadFxml();
+		initTableColumns();
 	}
 
 	public Pane getPane() {
-		
+
 		return rootPane;
 	}
 
@@ -104,7 +105,15 @@ public class CarRegistrationController {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	private void initTableColumns() {
+		columnBrand.setCellValueFactory(new PropertyValueFactory<>("carBrand"));
+		columnCompany.setCellValueFactory(new PropertyValueFactory<>("companyName"));
+		columnDate.setCellValueFactory(new PropertyValueFactory<>("createdTime"));
+		columnDocumentNo.setCellValueFactory(new PropertyValueFactory<>("documentNo"));
+		columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		columnLicense.setCellValueFactory(new PropertyValueFactory<>("carLicense"));
+		columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+	}
 
 }
--- src/test/java/com/ldselektronik/service/CarBrandServiceTest.java
+++ src/test/java/com/ldselektronik/service/CarBrandServiceTest.java
@@ -1,17 +1,11 @@
 package com.ldselektronik.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.ldselektronik.config.AppConfig;
 import com.ldselektronik.dto.CarBrandDTO;
-import com.ldselektronik.model.CarBrand;
 
 public class CarBrandServiceTest {
 
