cb072a9313d4372d3aab91c8a356c515dd205d5d
Baran Buzluk <baranbuzluk@gmail.com>
2021-10-23 14:30:10 +0300

    Loglama için util.Logger beanlarının üretilmesi sağlandı.

--- src/main/java/com/ldselektronik/config/AppConfig.java
+++ src/main/java/com/ldselektronik/config/AppConfig.java
@@ -1,14 +1,25 @@
 package com.ldselektronik.config;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "com.ldselektronik")
 @EnableAutoConfiguration
 @Import({ DatabaseConfig.class })
 public class AppConfig {
+
+	@Bean
+	@Scope("prototype")
+	public Logger produceLogger(InjectionPoint injectionPoint) {
+		return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+	}
 
 }
